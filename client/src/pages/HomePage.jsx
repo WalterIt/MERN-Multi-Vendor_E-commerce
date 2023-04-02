6cc9e5abe02dc02c8c2312766c7ce46e7c74a895
WalterIt <whiruba@gmail.com>
2023-04-02 19:46:20 -0300

    CLIENT => Created Hero and Categories Components

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/layout/Header";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
+      <Hero />
+      <Categories />
     </div>
   );
 };
